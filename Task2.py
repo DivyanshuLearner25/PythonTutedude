eac2ffef5a6ac1ca598024fd6a5ad9bb03d89265
Divyanshu Maurya <lakshya.kaushalindia@gmail.com>
2025-12-18 05:51:58 +0530

    Add user input and greeting functionality

--- Task2.py
+++ Task2.py
@@ -1,1 +1,4 @@
-
+first_name=input("Enter your first name :")
+last_name=input("Enter your last name :")
+concanate=first_name+" "+last_name
+print("Hello",concanate,"! Welcome to the Python program.")
