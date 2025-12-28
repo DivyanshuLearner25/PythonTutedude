9e8cbb293d56e50d8a952a18c508cfedb04b29ad
Divyanshu Maurya <lakshya.kaushalindia@gmail.com>
2025-12-28 07:47:55 +0530

    Update Task2.py

--- Assignment2/Task2.py
+++ Assignment2/Task2.py
@@ -1,1 +1,11 @@
 # This is my second task of Assignment 2.
+# To show the output of each iteration
+sum_of_numbers=0
+for j in range(1,51,1):
+    sum_of_numbers+=j
+    print(f"The sum of numbers from 1 to {j} is {sum_of_numbers}.")
+# Only Final Output
+sum_of_numbers=0
+for j in range(1,51,1):
+    sum_of_numbers+=j
+print(f"The sum of numbers from 1 to {j} is {sum_of_numbers}.")
