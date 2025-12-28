2fcb0b0a9db43428a276961c5cf87abe4bee755b
Divyanshu Maurya <lakshya.kaushalindia@gmail.com>
2025-12-28 06:39:54 +0530

    Update and rename Task1 to Task1.py

---
+++ Assignment1/Task1.py
@@ -0,0 +1,13 @@
+num1=int(input("Enter first number : "))
+num2=int(input("Enter second number : "))
+plus =num1+num2
+subtract=(num1-num2)
+multiply=num1*num2
+division=num1/num2
+#print separately
+"""print("Addition: ",plus)
+print("Subtraction: ",subtract)
+print("Multiplication:",multiply)
+print("Division:",division)"""
+#All output in one line
+print("Addition:",plus,"\n Subtraction:",subtract,"\nMultiplication:",multiply,"\nDivision:",division)
