18f2b750223f329513eb33e31268efdb5ac3b452
Divyanshu Maurya <lakshya.kaushalindia@gmail.com>
2025-12-28 07:19:42 +0530

    Add even-odd checker program

--- Assignment2/Task1.py
+++ Assignment2/Task1.py
@@ -1,1 +1,10 @@
 #This is my first task of Assignment 2.
+# Code
+print("This is a program to check whether the entered number by you is even or odd.")
+# Taking Input from user (an integer)
+user_input=int(input("Enter an integer number: "))
+#Checking condition
+if user_input % 2 == 0:
+    print(f"{user_input} is an even number.")
+else:
+    print(f"{user_input} is an odd number.")
